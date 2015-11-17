68400f9bedcbef3d53adfb11342e47237299d9a4
JoshuaBWT <joshua.barette@hotmail.fr>
2015-11-17 15:56:32 +0100

    step 9 & 10

--- lib/app.jsx
+++ lib/app.jsx
@@ -12,30 +12,33 @@ var Box = React.createClass({
       return {value: this.props.initialValue};
     },
   componentWillMount: function () {
-    return setInterval(this.updateState, 300);
+    //return setInterval(this.updateState, 300);
   },
   updateState : function() {
     return this.setState({value: (this.state.value == "X")? "O" : "X" });
   },
   handleClick : function() {
-    this.updateState();
+    //this.updateState();
+    this.props.handleClick(this.props.index)
   },
   render: function onRender () {
-    return (
-      <button onClick={this.handleClick} style={boxStyle}>{this.state.value}</button>
-    );
+    return ( <button onClick={this.handleClick} style={boxStyle}>{this.state.value}</button> );
   }
 });
 
 var Row = React.createClass({
+  handleClick : function(index) {
+    childData.updateState();
+  },
   render: function onRender () {
-    var ligns = ["-","-","-"];
-    //this.props.Lign;
+    var ligns = this.props.ligns;
     return (
       <div>
-        {ligns.map(function(lign) {
-          return <Box initialValue={lign} />
-        })}
+      {
+          ligns.map(function(lign, index) {
+            return <Box initialValue={lign} index={index} onClick={this.handleClick} />
+          }.bind(this))
+      }
       </div>
     );
   }
@@ -53,4 +56,4 @@ var Grid = React.createClass({
   }
 });
 
-React.render(<Row />, document.body);
+React.render(<Row ligns={["-","-","-"]}/>, document.body);
